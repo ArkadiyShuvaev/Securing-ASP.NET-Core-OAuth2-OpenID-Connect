fb419c9c2b6820ed282825a931346256f52a4482
Arkadiy Shuvaev <Arkadiy_Shuvaev@epam.com>
2017-11-23 09:38:21 +0300

    Integrated Facebook Authentication

--- src/Shuvaev.IDP/AppOptions.cs
+++ src/Shuvaev.IDP/AppOptions.cs
@@ -3,6 +3,14 @@
 	public class AppOptions
 	{
 		public ConnectionStringsCfg ConnectionStrings { get; set; }
+		public FacebookConfiguration Facebook { get; set; }
+
+		public class FacebookConfiguration
+		{
+			public string AppId { get; set; }
+			public string AppSecret { get; set; }
+		}
+
 		public class ConnectionStringsCfg
 		{
 			public string IdpUserDbConnection { get; set; }
--- src/Shuvaev.IDP/Startup.cs
+++ src/Shuvaev.IDP/Startup.cs
@@ -1,11 +1,15 @@
 ﻿using AutoMapper;
+using IdentityServer4;
 using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Shuvaev.IDP.Controllers.UserRegistration;
 using Shuvaev.IDP.Entities;
 using Shuvaev.IDP.Services;
@@ -28,6 +32,8 @@ namespace Shuvaev.IDP
 			services.AddOptions();
 			services.Configure<AppOptions>(_configuration);
 
+	        var config = _configuration.Get<AppOptions>();
+
 			var connectionString = _configuration.GetConnectionString("idpUserDbConnection");
 
 			//var connectionString = "test";
@@ -46,6 +52,16 @@ namespace Shuvaev.IDP
 				.AddInMemoryIdentityResources(Config.GetIdentityResources())
 				.AddInMemoryApiResources(Config.GetApiResources())
 				.AddInMemoryClients(Config.GetClients());
+
+	        services.AddAuthentication() //CookieAuthenticationDefaults.AuthenticationScheme
+										 //.AddCookie(o => o.LoginPath = new PathString("/login"))
+				.AddFacebook("FacebookScheme", "Facebook", o =>
+		        {
+			        o.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+					o.AppId = config.Facebook.AppId;
+			        o.AppSecret = config.Facebook.AppSecret;
+		        });
+
 		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,14 +82,14 @@ namespace Shuvaev.IDP
 		        cfg.CreateMap<RegisterUserViewModel, User>()
 					.ForMember(d => d.Username, opt => opt.MapFrom(s => s.UserName));
 			});
-				
-				
+			
 
 	        userContext.EnsureSeedDataForContext();
 
 
 			app.UseIdentityServer();
-	        app.UseStaticFiles();
+	        
+			app.UseStaticFiles();
 	        app.UseMvcWithDefaultRoute();
 
         }
